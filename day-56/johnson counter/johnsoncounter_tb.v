9b116e1dd8e7faf2f8df53e1f9e95c73c345a8d2
Sai-talluri-55 <137263980+Sai-talluri-55@users.noreply.github.com>
2023-12-10 19:11:06 +0530

    Add files via upload

---
+++ day-56/johnson counter/johnsoncounter_tb.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10.12.2023 19:05:30
+// Design Name: 
+// Module Name: johnsoncounter_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module johnsoncounter_tb(
+
+    );
+    reg clk,rst;
+wire [3:0]q;
+johnson_counter dut(clk,rst,q);
+initial clk=0;
+always #5 clk=~clk;
+initial 
+begin
+rst=1;
+#20 rst=0;
+#100 $finish;
+end
+endmodule
